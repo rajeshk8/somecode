602713aff4520bb299e144f0ac63cc0ea19b92c1
rajeshk8@yahoo.com <rajeshk8@yahoo.com>
2020-01-30 07:33:27 +0000

    d

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
-NUMBER=$((NUMBERONE + NUMBERTWO))
+#NUMBER=$((NUMBERONE + NUMBERTWO))
 echo "$NUMBER"
 echo "completed"
